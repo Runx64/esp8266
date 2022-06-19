7761ce575bdeaae3236b0898b13c095a5a6a1247
Runx64 <106265310+Runx64@users.noreply.github.com>
2022-06-19 21:04:29 +0700

    Add files via upload

---
+++ sampleBlynkWriteV2.ino
@@ -0,0 +1,45 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "TMPLjfyHpJB2"
+#define BLYNK_DEVICE_NAME "swDevice"
+#define BLYNK_AUTH_TOKEN "ZxCRZvkQ1bZvWSrKiE6gQ_JbkZ9WcDnG"
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Orange_2.4G";
+char pass[] = "0829887580";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  pinMode(D1,OUTPUT);
+  pinMode(D2,OUTPUT);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
+BLYNK_WRITE(V2)
+    {
+      if(param.asInt()) {
+        Serial.println("LED ON");
+        digitalWrite(D1,HIGH);
+        digitalWrite(D2,HIGH);
+      }
+      else{
+        Serial.println("LED OFF");
+        digitalWrite(D1,LOW);
+        digitalWrite(D2,LOW);
+      }
+    }
