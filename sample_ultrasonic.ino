a22e39b35c5464f0c200f8e84d2e479f1064d6ca
Runx64 <106265310+Runx64@users.noreply.github.com>
2022-06-29 13:46:29 +0700

    Add files via upload

---
+++ sample_ultrasonic.ino
@@ -0,0 +1,29 @@
+#define trig D1 //กำหนด Trig pin เป็น D1
+#define echo D2 //กำหนด Echo pin เป็น D2
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int cm = checkDistance(); //เรียกใช้ฟังก์ชัน checkDistance
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(200);
+}
+
+//ฟังก์ชันสำหรับคำนวณระยะทางจาก Ultrasonic
+int checkDistance() {
+  int duration, cm;
+  pinMode(trig, OUTPUT);
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trig, LOW);
+  pinMode(echo, INPUT);
+  duration = pulseIn(echo, HIGH);
+  cm = duration / 29 / 2;
+  return cm; //ส่งค่าระยะทางจากการคำนวณกลับไปฟังก์ชันหลัก
+}
